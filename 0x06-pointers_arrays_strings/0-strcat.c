b1da4fcea919d8f464e8c232aa2e00d69d32c931
mercy-kalondu <mercypeterkalo@gmail.com>
2023-03-01 12:05:29 -0800

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,14 +8,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int dest_len = strlen(dest);
-int i;
+	int i;
+	int j;
 
-for (i = 0; src[i] != '\0'; i++)
-{
-dest[dest_len + i] = src[i];
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-}
-dest[dest_len + i] = '\0';
-return (dest);
+	dest[i] = '\0';
+	return (dest);
 }
