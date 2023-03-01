2a0b704dca634f6df377bd048df9881c61e30624
mercy-kalondu <mercypeterkalo@gmail.com>
2023-03-01 12:14:40 -0800

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,13 +10,17 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int dest_len = strlen(dest);
-int i;
+int i, co;
 
-for (i = 0; i < n && src[i] != '\0'; i++)
+for (i = 0; dest[i] != '\0'; i++)
 {
-dest[dest_len + i] = src[i];
 }
-dest[dest_len + i] = '\0';
+
+for (co = 0; co < n; co++)
+{
+dest[i + co] = src[co];
+if (src[co] == '\0')
+co = n;
+}
 return (dest);
 }
