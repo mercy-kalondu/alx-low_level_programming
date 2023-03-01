94698c9b942ac448dde2394324d45c2947208d13
mercy-kalondu <mercypeterkalo@gmail.com>
2023-03-01 11:57:15 -0800

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+int dest_len = strlen(dest);
+int i;
+
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[dest_len + i] = src[i];
+
+}
+dest[dest_len + i] = '\0';
+return (dest);
+}
