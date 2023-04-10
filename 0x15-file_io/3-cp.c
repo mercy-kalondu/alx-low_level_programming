36ba1b4fc81550c79dacf84aa16c07a840421326
mercy-kalondu <mercypeterkalo@gmail.com>
2023-04-10 01:28:07 -0700

    copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -40,47 +40,47 @@ exit(100);
  * @argv: An array of pointers assigned to the arguments.
  * Return: 0 on success.
  * Description:argument count incorrect - 97.
- *              file_f does not exist or  read - 98.
- *              file_t not created or written - 99.
- *              file_t or file_f not  closed - 100.
+ *              file_from does not exist or  read - 98.
+ *              file_to not created or written - 99.
+ *              file_to or file_f not  closed - 100.
  */
 int main(int argc, char *argv[])
 {
-int f;
-int t;
+int from;
+int to;
 int r;
 int w;
 char *buffer;
 if (argc != 3)
 {
-dprintf(STDERR_FILENO, "Usage: cp file_f file_t\n");
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
 buffer = create_buffer(argv[2]);
-f = open(argv[1], O_RDONLY);
+from = open(argv[1], O_RDONLY);
 r = read(f, buffer, 1024);
-t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 do {
-if (f == -1 || r == -1)
+if (from == -1 || r == -1)
 {
 dprintf(STDERR_FILENO,
 "Error: Can't read from file %s\n", argv[1]);
 free(buffer);
 exit(98);
 }
-w = write(t, buffer, r);
-if (t == -1 || w == -1)
+w = write(to, buffer, r);
+if (to == -1 || w == -1)
 {
 dprintf(STDERR_FILENO,
 "Error: Can't write to %s\n", argv[2]);
 free(buffer);
 exit(99);
 }
-r = read(f, buffer, 1024);
-t = open(argv[2], O_WRONLY | O_APPEND);
+r = read(from, buffer, 1024);
+to = open(argv[2], O_WRONLY | O_APPEND);
 } while (r > 0);
 free(buffer);
-close_file(f);
-close_file(t);
+close_file(from);
+close_file(to);
 return (0);
 }
