9e26c0ef60cc2f0e866c02ecab006a88cea62d9b
mercy-kalondu <mercypeterkalo@gmail.com>
2023-04-10 01:36:33 -0700

    copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 char *create_buffer(char *file);
-void close_file(int av);
+void close_file(int fd);
 /**
  * create_buffer - 1024 bytes are allocated to buffer.
  * @file: name of the file buffer is storing chars for.
@@ -22,15 +22,15 @@ return (buffer);
 }
 /**
  * close_file - file descriptors are closed
- * @av: The file descriptor to be closed.
+ * @fd: The file descriptor to be closed.
  */
-void close_file(int av)
+void close_file(int fd)
 {
 int b;
-b = close(av);
+b = close(fd);
 if (b == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close av %d\n", av);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 exit(100);
 }
 }
@@ -48,8 +48,8 @@ int main(int argc, char *argv[])
 {
 int from;
 int to;
-int r;
-int w;
+int a;
+int c;
 char *buffer;
 if (argc != 3)
 {
@@ -58,27 +58,27 @@ exit(97);
 }
 buffer = create_buffer(argv[2]);
 from = open(argv[1], O_RDONLY);
-r = read(f, buffer, 1024);
+r = read(from, buffer, 1024);
 to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 do {
-if (from == -1 || r == -1)
+if (from == -1 || a == -1)
 {
 dprintf(STDERR_FILENO,
 "Error: Can't read from file %s\n", argv[1]);
 free(buffer);
 exit(98);
 }
-w = write(to, buffer, r);
-if (to == -1 || w == -1)
+w = write(to, buffer, a);
+if (to == -1 || c == -1)
 {
 dprintf(STDERR_FILENO,
 "Error: Can't write to %s\n", argv[2]);
 free(buffer);
 exit(99);
 }
-r = read(from, buffer, 1024);
+a = read(from, buffer, 1024);
 to = open(argv[2], O_WRONLY | O_APPEND);
-} while (r > 0);
+} while (c > 0);
 free(buffer);
 close_file(from);
 close_file(to);
